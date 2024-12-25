dd0a6bec2bb07d20d6d774f97aaadf7e0b482ee6
adithya1107 <adigaadithya70@gmail.com>
2024-12-25 16:45:53 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogs = require('./api/blogsData.json')
-const port = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const blogs = require('./api/blogsData.json');
+const port = process.env.PORT || 5005;
 
-// middleware
-app.use(cors())
-app.use(express.json())
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Blog server is running!")
+// Serve static files from the frontend build directory
+app.use(express.static(path.join(__dirname, 'public'))); // 'public' is where your frontend build files are located
+
+// API Endpoints
+app.get('/blogs', (req, res) => {
+  res.send(blogs);
 });
 
-app.get('/blogs', (req, res) => {
-  res.send(blogs)
-})
 app.get('/blogs/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  // console.log(id)
-  const blog = blogs.filter(b => b.id === id);
-  // console.log(blog)
-  res.send(blog)
-})
+  const blog = blogs.filter((b) => b.id === id);
+  res.send(blog);
+});
 
+// Catch-all route to serve the frontend's index.html for unmatched routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
